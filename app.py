0cb781259b2d8d4c2c5a988f4b0bbae23968f485
redamalki652-debug <redamalki652@gmail.com>
2026-08-25 18:42:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def call_groq(messages):
     try:
         chat_completion = client.chat.completions.create(
             messages=messages,
-            model="gemma2-9b-it", # <<<<<< الموديل الجديد اللي خدام
+            model="llama-3.1-8b-instant", # <<<<<< الموديل الجديد
             temperature=0.7,
             max_tokens=2048
         )
